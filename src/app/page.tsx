a91ace27fcc30a046ae84c81c9bd58f510e83751
kevin4052 <kevin.hernandez001@gmail.com>
2024-04-12 22:08:24 -0500

    sending event messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,70 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
+
+type Event = {
+    type: string;
+    payload: string;
+};
 
 export default function Home() {
-    const connection = useRef(null);
-    const [usernameCount, setUsernameCount] = useState(12);
-    const [username, setUsername] = useState("");
+    const [socket, setSocket] = useState<WebSocket | undefined>(undefined);
+    const [inputMessage, setInputMessage] = useState<string>("");
 
-    useEffect(() => {
-        const socket = new WebSocket(`ws://localhost:8080/ws`);
-
-        socket.addEventListener("open", (event) => {
-            socket.send("connection established");
-        });
-
-        socket.addEventListener("message", (event) => {
-            console.log("Msg from server", event.data);
-        });
-
-        socket.addEventListener("close", (event) => {
-            console.log("connection closed");
-        });
-    }, []);
-
-    const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInuptMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
-        setUsername(value);
-        setUsernameCount(12 - value.length);
+        setInputMessage(value);
     };
 
-    const connectWS = () => {
-        const ws = new WebSocket(`ws://localhost:8080/ws`);
-        ws.send("from FLOP");
+    const handleConnectingSocket = () => {
+        const websocket = new WebSocket("ws://localhost:8080/ws");
+
+        websocket.addEventListener("open", () => {
+            sendEvent("send_message", "NEW CONNECTION");
+        });
+
+        websocket.addEventListener("close", () => {
+            console.log("connection closed");
+        });
+
+        websocket.addEventListener("message", (event: any) => {
+            console.log(event);
+        });
+
+        setSocket(websocket);
+    };
+
+    useEffect(() => {
+        handleConnectingSocket();
+    }, []);
+
+    const sendMessage = () => {
+        sendEvent("send_message", inputMessage);
+        setInputMessage("");
+    };
+
+    const sendEvent = (eventType: string, payload: string) => {
+        const event: Event = {
+            type: eventType,
+            payload: payload,
+        };
+        socket?.send(JSON.stringify(event));
     };
 
     return (
         <div className="w-full min-w-80 max-w-96 h-96 mx-auto border-indigo-900">
-            <form className="mt-4">
-                <div className="flex flex-col font-semibold text-lg mb-2">
-                    <label className="mb-1 px-2">ROOM CODE</label>
-                    <input
-                        className="rounded-xl border-2 px-2 py-1"
-                        placeholder="ENTER 4-LETTER CODE"
-                        maxLength={4}
-                        type="text"
-                    ></input>
-                </div>
-                <div className="flex flex-col font-semibold text-lg">
-                    <label className="flex flex-row justify-between mb-1 px-2">
-                        NAME
-                        <span>{usernameCount}</span>
-                    </label>
-                    <input
-                        className="rounded-xl border-2 px-2 py-1 mb-2"
-                        placeholder="ENTER YOUR NAME"
-                        maxLength={12}
-                        type="text"
-                        onChange={handleUsername}
-                        value={username}
-                    ></input>
-                </div>
-                <div className="flex flex-row justify-center">
-                    <button className="bg-slate-500 w-full mx-6 my-2 py-2 font-semibold text-lg rounded-xl">
-                        PLAY
-                    </button>
-                </div>
-            </form>
             <div>
-                <button onClick={connectWS}>connect</button>
+                <input
+                    className="border border-black px-4 py-2 m-2"
+                    type="text"
+                    value={inputMessage}
+                    onChange={handleInuptMessage}
+                ></input>
+                <button
+                    className="border border-black px-4 py-2 mx-2"
+                    onClick={sendMessage}
+                >
+                    Send Message
+                </button>
             </div>
         </div>
     );
