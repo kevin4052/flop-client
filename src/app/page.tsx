442ed53a7c29099801619483b7e2fc34bbe8f2ea
kevin4052 <kevin.hernandez001@gmail.com>
2024-04-16 13:10:08 -0500

    testing websocket connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 type Event = {
     type: string;
@@ -9,6 +9,7 @@ type Event = {
 export default function Home() {
     const [socket, setSocket] = useState<WebSocket | undefined>(undefined);
     const [inputMessage, setInputMessage] = useState<string>("");
+    const [isConnected, setIsConnected] = useState<boolean>(false);
 
     const handleInuptMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
@@ -16,26 +17,29 @@ export default function Home() {
     };
 
     const handleConnectingSocket = () => {
+        if (isConnected) {
+            socket?.close();
+            return;
+        }
+
         const websocket = new WebSocket("ws://localhost:8080/ws");
-
-        websocket.addEventListener("open", () => {
+        websocket.onopen = () => {
             sendEvent("send_message", "NEW CONNECTION");
-        });
+            setIsConnected(true);
+        };
 
-        websocket.addEventListener("close", () => {
-            console.log("connection closed");
-        });
+        websocket.onclose = () => {
+            console.log("Connection closed");
+            setSocket(undefined);
+            setIsConnected(false);
+        };
 
-        websocket.addEventListener("message", (event: any) => {
+        websocket.onmessage = (event: any) => {
             console.log(event);
-        });
+        };
 
         setSocket(websocket);
     };
-
-    useEffect(() => {
-        handleConnectingSocket();
-    }, []);
 
     const sendMessage = () => {
         sendEvent("send_message", inputMessage);
@@ -52,9 +56,15 @@ export default function Home() {
 
     return (
         <div className="w-full min-w-80 max-w-96 h-96 mx-auto border-indigo-900">
+            <div className="flex flex-col justify-center">
+                <h3>Connected: {isConnected ? "true" : "false"}</h3>
+                <button onClick={handleConnectingSocket}>
+                    {!isConnected ? "Connect" : "Disconnect"}
+                </button>
+            </div>
             <div>
                 <input
-                    className="border border-black px-4 py-2 m-2"
+                    className="border border-black px-4 py-2 m-2 w-full"
                     type="text"
                     value={inputMessage}
                     onChange={handleInuptMessage}
