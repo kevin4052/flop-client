3900a0e584015b80599719dde1c0e4f8b33e94bc
kevin4052 <kevin.hernandez001@gmail.com>
2024-04-20 06:34:18 -0500

    navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavBar() {
     return (
         <nav className="flex flex-row justify-center items-center bg-sky-700 h-50">
-            <div className="font-mono text-6xl font-bold">FLOP</div>
+            <div className="font-mono text-6xl font-bold py-4">FLOP</div>
         </nav>
     );
 }
